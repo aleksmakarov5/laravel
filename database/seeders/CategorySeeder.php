87b73f2c79467651aaea99d7ac7f4e45b761a1d9
Alex Makarov <aleksmakarov5@yandex.ru>
2021-04-22 23:51:01 +0300

    Created Seeders categories

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Facades\DB;
+
+
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++){
+            DB::table('categories')->insert([
+                'title' => 'Category '.$i,
+                'slug' => 'category-'.$i,
+            ]);
+        }
+    }
+}
